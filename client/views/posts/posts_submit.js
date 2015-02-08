3f9400e0b88740421d591f4166eb425bb3c7fc03
Shaahin Shahbazi <shaahin@gmail.com>
2015-02-07 18:41:35 -0600

    Using server-side method: Block repetitious URLs

--- client/views/posts/posts_submit.js
+++ client/views/posts/posts_submit.js
@@ -8,8 +8,17 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val(),
             message: $(e.target).find('[name=message]').val()
         }
-        post._id = Posts.insert(post);
+        
+        // The Meteor.call function calls a Method named by its first argument. 
+        // You can provide arguments to the call (in this case, the post object 
+        // we constructed from the form), and finally attach a callback, which 
+        // will execute when the server-side Method is done
+        Meteor.call('post', post, function(error, result) 
+          { if (error)
+              return alert(error.reason);
+                                                       
         // Router's go() function will use id to construct a URL for us to browse to.
-        // Router.go('postPage', post);
+        Router.go('postPage', {_id: result._id});
+        });
     }
 });
