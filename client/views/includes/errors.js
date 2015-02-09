c447cf31ebe16e2513da95d54313055343923c93
Shaahin Shahbazi <shaahin@gmail.com>
2015-02-09 14:35:08 -0600

    Error msg clearance (using setTimeout)

--- client/views/includes/errors.js
+++ client/views/includes/errors.js
@@ -3,3 +3,12 @@ Template.errors.helpers({
         return Errors.find();
     }
 });
+
+// A template's rendered callback triggers every time it's rendered in the browser.
+
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+};
