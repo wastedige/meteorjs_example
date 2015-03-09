55f399b0b7f8220bbf3deb3ce82547ed7d3dfe6b
Shaahin Shahbazi <shaahin@gmail.com>
2015-03-08 20:36:38 -0500

    Submit comments

--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,26 @@
 Comments = new Meteor.Collection('comments');
+
+// checking that the user is logged in, that the comment has a body, and that it's linked to a post.
+Meteor.methods({
+  commentInsert: function(commentAttributes) {
+    check(this.userId, String);
+    check(commentAttributes, {
+      postId: String,
+      body: String
+    });
+    
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+
+    if (!post)
+      throw new Meteor.Error('invalid-comment', 'You must comment on a post');
+    
+    comment = _.extend(commentAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date()
+    });
+    
+    return Comments.insert(comment);
+  }
+});
